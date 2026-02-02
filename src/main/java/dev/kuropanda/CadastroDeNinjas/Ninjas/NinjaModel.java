a62afc5045461068ce34235a2b866a0c7f559ebf
MarcusVRocha <marcus.cavalcanterocha@gmail.com>
2026-02-02 14:07:12 -0300

    feat: Adiciona lombok nas classes para abstração

--- src/main/java/dev/kuropanda/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/kuropanda/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -3,14 +3,15 @@ package dev.kuropanda.CadastroDeNinjas.Ninjas;
 
 import dev.kuropanda.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
-
-//Entity -> Transforma classe em entidade para o DB
-// JPA = Java Persistence API
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -22,39 +23,7 @@ public class NinjaModel {
 
     // @ManyToTone - Um ninja para uma unica missão
     @ManyToOne
-    @JoinColumn(name = "missoes_id") // Foreign Key - Chave Estrangeira
+    @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
