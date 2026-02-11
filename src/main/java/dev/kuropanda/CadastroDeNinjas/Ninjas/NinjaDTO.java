7bc9ceae1beb4c8e942ac74b6db66182a5bfc71e
MarcusVRocha <marcus.cavalcanterocha@gmail.com>
2026-02-11 16:29:24 -0300

    Chore: Inclui a versao do DTO pela entidadade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/dev/kuropanda/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/kuropanda/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,19 @@
 package dev.kuropanda.CadastroDeNinjas.Ninjas;
+import dev.kuropanda.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private String rank;
+    private int idade;
+    private MissoesModel missoes;
 }
